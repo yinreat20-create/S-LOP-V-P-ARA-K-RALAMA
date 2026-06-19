bf1628c0d84f00cef5765ca0c1b8af3ceded98a3
yinreat20-create <yinreat20@gmail.com>
2026-06-19 15:48:03 +0300

    Update vite.config.ts

--- project/vite.config.ts
+++ project/vite.config.ts
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/S-LOP-V-P-ARA-K-RALAMA/', // GitHub Pages alt klasör yolu eklendi
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
